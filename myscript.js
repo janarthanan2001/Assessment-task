684b74738ca3306914173740fd811cededa404b5
Janarthanan A <54324117+janarthanan2001@users.noreply.github.com>
2022-12-29 23:17:50 +0530

    final commit

---
+++ myscript.js
@@ -0,0 +1,16 @@
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
+
+customBtn.addEventListener("click", function() {
+  realFileBtn.click();
+});
+
+realFileBtn.addEventListener("change", function() {
+  if (realFileBtn.value) {
+    customTxt.innerHTML = realFileBtn.value;
+  } 
+  else {
+    customTxt.innerHTML = "No file chosen, yet.";
+  }
+});
